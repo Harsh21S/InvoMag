cb7ad7387163f7008c83b71808587880de6dd8f7
Harsh Sekukar <harshxofficialz@gmail.com>
2024-11-17 03:54:04 +0530

    front end connected to backend

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,9 +1,11 @@
-
+"use client";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      Dashboard
+    </div>
+  );
+};
 
 export default Dashboard;
