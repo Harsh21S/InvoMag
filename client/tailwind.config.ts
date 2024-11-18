da43e3f811f8cc2b219190b0a190ed4d975adc50
Harsh Sekukar <harshxofficialz@gmail.com>
2024-11-19 04:33:32 +0530

    more optimization

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -56,8 +56,8 @@ const themes = {
 const config: Config = {
   darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
